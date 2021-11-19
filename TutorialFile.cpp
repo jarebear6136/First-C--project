24de51f93a1e16262919d05c8b394f4d4ef74871
Jarrett Nobles <noblesjarrett035@gmail.com>
2021-11-19 12:04:14 -0500

    Added data types

--- TutorialFile.cpp
+++ TutorialFile.cpp
@@ -2,8 +2,21 @@
 using namespace std;
 
 int main(){
+    //print statements
     cout << "Hello World!"<< endl;
     cout<<"C++ Project1";
     return 0;
+    //tutorial files only
+    //data type examples
+    int myNum= 5;
+    double floatNum=5.99;
+    char myLetter= 'J';
+    string MyString="String 1";
+    bool BoolValue=true;
+    int x =5, y=6, z=7;
+    cout<< x + y + z;
+    
+
+
 
 }
