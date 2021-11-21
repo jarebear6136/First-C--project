e24b3b87b71ce84e1b2c2de21ed7f1ddf8accec4
Jarrett Nobles <noblesjarrett035@gmail.com>
2021-11-20 23:31:18 -0500

    Another tutorial file

---
+++ YouTubeTutorial.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //test run
+    cout << "Hello World \n"; 
+
+    //declare integer x
+    int x;
+    //user types a number
+    cout << "Type a number";
+    //get user info from the keyboard
+    cin >>x; 
+    //display input value
+    cout << "Your Number is: " <<x; 
+}
